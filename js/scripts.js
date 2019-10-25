4d442b713db6b4c2eb05563622a8388948477b52
Geoffrey Orengo <geoff.orengo@yahoo.com>
2019-10-25 12:46:35 -0700

    add var values in scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+var animal = $("input:radio[name=animal]:checked").val();
+
+var time = $("input:radio[name=time]:checked").val();
+
+var problem = $("input:radio[name=problem]:checked").val();
+
+var frustrate = $("input:radio[name=frustrate]:checked").val();
+
 var color = $("input:radio[name=color]:checked").val();
