eb53944090fe8c3350ae0ab2e62569218a659309
Geoffrey Orengo <geoff.orengo@yahoo.com>
2019-10-25 14:04:44 -0700

    modify js scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
-$(document).ready(function() { 
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
 
-var animal = $("input:radio[name=animal]:checked").val();
+    var animal = $("input:radio[name=animal]:checked").val();
 
-var time = $("input:radio[name=time]:checked").val();
+    var time = $("input:radio[name=time]:checked").val();
 
-var problem = $("input:radio[name=problem]:checked").val();
+    var problem = $("input:radio[name=problem]:checked").val();
 
-var frustrate = $("input:radio[name=frustrate]:checked").val();
+    var frustrate = $("input:radio[name=frustrate]:checked").val();
 
-var color = $("input:radio[name=color]:checked").val();
+    var color = $("input:radio[name=color]:checked").val();
 
+    if (animal === cat || animal === dog) {
+      
+    }
+
+  });
 });
