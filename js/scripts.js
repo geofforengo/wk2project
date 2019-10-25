266adcd39bc91eda8cceb824275c58962a1a6c30
Geoffrey Orengo <geoff.orengo@yahoo.com>
2019-10-25 13:41:16 -0700

    add submit button class and modifications to jsscripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+$(document).ready(function() { 
+
 var animal = $("input:radio[name=animal]:checked").val();
 
 var time = $("input:radio[name=time]:checked").val();
@@ -7,3 +9,5 @@ var problem = $("input:radio[name=problem]:checked").val();
 var frustrate = $("input:radio[name=frustrate]:checked").val();
 
 var color = $("input:radio[name=color]:checked").val();
+
+});
